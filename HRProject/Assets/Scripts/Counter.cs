4a69f91abde828ab12c5e4fbf7a97af4eb8b82ce
Jarod Huber <j4r0dhub3r@gmail.com>
2023-03-21 14:58:42 -0700

    Jarod Huber - added PlayerMotor, refitted for InputSystem, feels a little jank might have to cut move by axis

---
+++ HRProject/Assets/Scripts/Counter.cs
@@ -0,0 +1,280 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Counter
+{
+    [Tooltip("the Maximum value this counter can reach")]
+    [SerializeField]
+    protected float max;
+    protected float cur;
+
+    /// <summary>
+    /// Max value of the counter
+    /// </summary>
+    public float Max { get => max; }
+    /// <summary>
+    /// The current value of the counter
+    /// </summary>
+    public float Cur { get => cur; }
+
+    /// <summary>
+    /// The percent left in the counter
+    /// </summary>
+    public float PercentComplete
+    {
+        get => cur / max;
+    }
+    /// <summary>
+    /// how much is left in the counter
+    /// </summary>
+    public float AmountRemaining
+    {
+        get => max - cur;
+    }
+
+    public Counter()
+    {
+        cur = 0;
+        max = 0;
+    }
+
+    /// <summary>
+    /// Creates a counter where Cur is set to 0 and the length is max
+    /// </summary>
+    /// <param name="max">Max value of the counter</param>
+    public Counter(float max)
+    {
+        this.cur = 0;
+        this.max = max;
+    }
+
+
+    public void SetMax(float newMax, bool resetCur = false)
+    {
+        max = newMax;
+
+        if (resetCur)
+            cur = 0.0f;
+    }
+
+    /// <summary>
+    /// Reset the counter to the startPoint
+    /// </summary>
+    /// <param name="startPoint">The value you want the counter to get set to</param>
+    public void Reset(float startPoint = 0)
+    {
+        cur = startPoint;
+    }
+
+    /// <summary>
+    /// Increases counter by 1
+    /// </summary>
+    public virtual void Count()
+    {
+        ++cur;
+        cur = Mathf.Clamp(cur, 0, max);
+    }
+
+    /// <summary>
+    /// Increases counter by value
+    /// </summary>
+    /// <param name="value">The float value you want to add to counter</param>
+    public void Count(float value)
+    {
+        cur += value;
+        cur = Mathf.Clamp(cur, 0, max);
+    }
+
+    /// <summary>
+    /// Lerps towards Max from 0
+    /// </summary>
+    /// <param name="increment">how far towards Max Cur is</param>
+    public void Lerp(float increment)
+    {
+        cur = max * increment;
+    }
+    /// <summary>
+    /// Lerps towards Max from min
+    /// </summary>
+    /// <param name="min">value Cur starts at for the lerp</param>
+    /// <param name="increment">how far towards Max Cur is</param>
+    public void Lerp(float min, float increment)
+    {
+        cur = min + ((max - min) * increment);
+    }
+    /// <summary>
+    /// Lerps towards max from min
+    /// </summary>
+    /// <param name="min">value Cur starts at for the lerp</param>
+    /// <param name="max">value Cur lerps towards cannot be larger than Max</param>
+    /// <param name="increment">how far towards max Cur is</param>
+    public void Lerp(float min, float max, float increment)
+    {
+        if (max > this.max)
+        {
+            Debug.LogWarning("lerp max is greater than counter max, clamping to counter max");
+            max = this.max;
+        }
+
+        cur = min + ((max - min) * increment);
+    }
+    /// <summary>
+    /// Lerps away from Max towards 0
+    /// </summary>
+    /// <param name="increment">how far away from Max Cur is</param>
+    public void InverseLerp(float increment)
+    {
+        cur = max + (-max * increment);
+    }
+    /// <summary>
+    /// Lerps away from Max towards min
+    /// </summary>
+    /// <param name="min">value Cur lerps towards</param>
+    /// <param name="increment">how far towards min Cur is</param>
+    public void InverseLerp(float min, float increment)
+    {
+        cur = max + ((min - max) * increment);
+    }
+    /// <summary>
+    /// Lerps away from max towards min
+    /// </summary>
+    /// <param name="min">value Cur lerps towards</param>
+    /// <param name="max">value Cur lerps away from cannot be larger than Max</param>
+    /// <param name="increment">how far towards min Cur is</param>
+    public void InverseLerp(float min, float max, float increment)
+    {
+        if (max > this.max)
+        {
+            Debug.LogWarning("lerp max is greater than counter max, clamping to counter max");
+            max = this.max;
+        }
+
+        cur = max + ((min - max) * increment);
+    }
+
+    /// <summary>
+    /// Checks to see if the counter has reached or passed the max
+    /// </summary>
+    /// <param name="resetOnTrue">Whether you want the counter to reset when true</param>
+    /// <returns>Returns true if cur is greater than or equal to max</returns>
+    public bool IsComplete(bool resetOnTrue = true)
+    {
+        if (cur >= max)
+        {
+            if (resetOnTrue)
+                Reset();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Checks whether the counter has reached or passed the max and if not count up
+    /// </summary>
+    /// <param name="resetOnTrue">Whether you want the counter to reset when it returns true</param>
+    /// <returns>Returns true if cur is greater than or equal to max</returns>
+    public bool Check(bool resetOnTrue = true)
+    {
+        if (IsComplete(resetOnTrue))
+            return true;
+
+        Count();
+
+        return false;
+    }
+    /// <summary>
+    /// Checks whether the counter has reached or passed the max and if not count up by value
+    /// </summary>
+    /// <param name="value">value to count up by</param>
+    /// <param name="reset">Whether you want the counter to reset when it returns true</param>
+    /// <returns>Returns true if cur is greater than or equal to max</returns>
+    public bool Check(float value, bool reset = true)
+    {
+        if (IsComplete(reset))
+            return true;
+
+        Count(value);
+
+        return false;
+    }
+
+    /// <summary>
+    /// Counts up by the value then checks whether the counter has reached or passed the max
+    /// </summary>
+    /// <param name="reset">Whether you want the counter to reset when it returns true</param>
+    /// <returns>Returns true if cur is greater than or equal to max</returns>
+    public bool PreCheck(bool reset = true)
+    {
+        Count();
+
+        if (IsComplete(reset))
+            return true;
+
+        return false;
+    }
+    /// <summary>
+    /// Counts up by the value then checks whether the counter has reached or passed the max
+    /// </summary>
+    /// <param name="value">value to count up by</param>
+    /// <param name="reset">Whether you want the counter to reset when it returns true</param>
+    /// <returns>Returns true if cur is greater than or equal to max</returns>
+    public bool PreCheck(float value, bool reset = true)
+    {
+        Count(value);
+
+        if (IsComplete(reset))
+            return true;
+
+        return false;
+    }
+
+    public static implicit operator bool(Counter counter)
+    {
+        return counter.cur >= counter.max;
+    }
+}
+/// <summary>
+/// Used as an actual timer or can be used as a counter
+/// </summary>
+[System.Serializable]
+public class Timer : Counter
+{
+    /// <summary>
+    /// The current value of the timer
+    /// </summary>
+    public float Time { get => cur; }
+    /// <summary>
+    /// Max value of the timer
+    /// </summary>
+    public float Delay { get => max; }
+    /// <summary>
+    /// how much time is left in the timer
+    /// </summary>
+    public float TimeRemaining
+    {
+        get => max - cur;
+    }
+
+    public Timer() : base()
+    {
+    }
+
+    /// <summary>
+    /// Creates a counter where Cur is set to 0 and the length is max
+    /// </summary>
+    /// <param name="max">Max value of the counter</param>
+    public Timer(float max) : base(max)
+    {
+    }
+
+    /// <summary>
+    /// Increases timer by Time.deltaTime
+    /// </summary>
+    public override void Count()
+    {
+        cur += UnityEngine.Time.deltaTime;
+        cur = Mathf.Clamp(cur, 0, max);
+    }
+}
---
+++ HRProject/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,205 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMotor : MonoBehaviour
+{
+    // The CharacterController simply moves the player with repects to collision, this is Kinematic Movement as it will collide with objects but not react to forces
+    [SerializeField, Tooltip("This is the player's \"body\", it does the moving while respecting collision")]
+    private CharacterController _controller;
+    // This is the transform for the camera, for rotating the camera
+    [SerializeField, Tooltip("This is the camera that should be childed to the player object, the \"Game Camera\"")]
+    private Transform _head;
+    [SerializeField]
+    private InputActionMap _inputMap = new InputActionMap();
+
+    [Space(10)]
+    // Toggle for enabling and disabling movement
+    [SerializeField, Tooltip("Whether the player can or can't move / look")]
+    private bool _lockplayerController = false;
+
+    [Header("Move Variables")]
+    // How fast the player moves
+    [SerializeField, Tooltip("How fast the player moves when walking")]
+    private float _moveSpeed = 3.0f;
+    [SerializeField]
+    private float _jumpHeight = 1.5f;
+    // SnapDist is for a raycast so the player will smoothly slide down ramps
+    [SerializeField, Tooltip("When the player stops touching the ground for a frame they will attempt to snap to any object that is this many units below theeir feet")]
+    private float _snapDist = .5f;
+    [SerializeField]
+    private Vector3 _gravityScale = Vector3.up;
+
+    [Header("Look Variables")]
+    // Multiplier for lookDest, increases the target position
+    [SerializeField]
+    private float _mouseSensitivity = 3.0f;
+    // How quickly the mouse moves towards lookDest
+    [SerializeField]
+    private float _lookSpeed = 0.5f;
+    // Mouse smoothing for inbetweens
+    [SerializeField]
+    private float _smoothing = 2.0f;
+    // Locks the rotation around the X axis (looking up and down)
+    [SerializeField, Tooltip("How far up (y) and down (x) they player can look")]
+    private Vector2 _vertLookExtents = new Vector2(-70.0f, 70.0f);
+
+    // Only needed if you need to access the head from another script and don't want to assign to there as well
+    public Transform Head => _head;
+    public float MoveSpeed { set => _moveSpeed = value; }
+    public float MouseSenMod { set => _mouseSenMod = value; }
+
+    private float SnapDist => (_controller.height / 2.0f) + _snapDist;
+    private Vector3 EffectiveGravity
+    {
+        get
+        {
+            Vector3 g = Physics.gravity;
+            g.Scale(_gravityScale);
+            return g;
+        }
+    }
+
+    // Important for falling
+    private float _yVel = 0.0f;
+    // For ground snapping
+    private bool _wasGrounded = false;
+    private bool _jumpWish = false;
+
+    private float _mouseSenMod;
+
+    // Vectors for rotation, they need to store information between frames
+    private Vector2 _lookVel, _lookPos;
+    // Without these the player rotation will always snap to (0, 0, 0)
+    private Quaternion _rotOffset, _rotOffsetHead;
+
+    private void Start()
+    {
+        // Set the rotation offsets
+        _rotOffset = transform.rotation;
+        _rotOffsetHead = _head.transform.localRotation;
+
+        _mouseSenMod = PlayerPrefs.GetFloat("Player.LookSensitivity", 1.0f);
+
+        _inputMap["Jump"].performed += AttemptJump;
+    }
+
+    private void Update()
+    {
+        //if (Input.GetButtonDown("Jump") && _controller.isGrounded)
+        //    _jumpWish = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_lockplayerController)
+            return;
+
+        Move();
+        Look();
+    }
+
+    private void AttemptJump(InputAction.CallbackContext context)
+    {
+        if (!_controller.isGrounded)
+            return;
+
+        _jumpWish = true;
+    }
+
+    private void Move()
+    {
+        // Grab the movement inputs and normalize them to avoid having diagonal movement faster than orthogonal
+        Vector3 vel = _inputMap["Move"].ReadValue<Vector2>();
+        vel.z = vel.y;
+        vel.y = 0;
+
+        print(vel);
+
+        // This augements the vector to be relative to the player (so if you press 'forward' it moves in the same direction as transform.forward), and multiply it by moveSpeed
+        vel = transform.TransformDirection(vel) * _moveSpeed;
+
+        // Apply gravity to yVel then assign it to vel
+        if (_jumpWish)
+        {
+            _jumpWish = false;
+
+            _yVel += Mathf.Sqrt(-2.0f * EffectiveGravity.y * _jumpHeight);
+            _wasGrounded = false;
+        }
+        _yVel += EffectiveGravity.y * Time.deltaTime;
+
+        vel.y += _yVel;
+
+        // This is will move the player while respecting collision, it will collide with walls and the floor, go up ramps (depending on the angle), and go up steps (depending on step height) 
+        _controller.Move(vel * Time.deltaTime);
+
+        GroundCheck();
+    }
+
+    private void GroundCheck()
+    {
+        // If you are grounded reset yVel and set wasGrounded for next frame
+        if (_controller.isGrounded)
+        {
+            _yVel = 0.0f;
+            _wasGrounded = true;
+            return;
+        }
+
+        // If the player is not grounded but was last frame do a Raycast and if it hits something snap to that thing
+        if (_wasGrounded && Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, SnapDist))
+        {
+            // Get the distance you have to travel to be snapped to the ground
+            float snapAmount = Vector3.Distance(hit.point, transform.position);
+            // Ignore half the height as the Raycast shoots from the center of the player
+            snapAmount -= _controller.height / 2.0f;
+            // Convert the amount you have to move movement to a velocity scaler
+            snapAmount /= Time.deltaTime;
+
+            _controller.Move(Vector3.down * snapAmount);
+
+            // Reset yVel
+            _yVel = 0.0f;
+            return;
+        }
+
+        // Set wasGrounded for next frame
+        _wasGrounded = false;
+    }
+
+    private void Look()
+    {
+        // get mouse input and apply scalers that are necessary for reasons I don't know but recognise
+        Vector2 lookDest = _mouseSenMod * _mouseSensitivity * _inputMap["Look"].ReadValue<Vector2>();
+
+        // smooth input
+        _lookVel.Set(Mathf.Lerp(_lookVel.x, lookDest.x, 1.0f / _smoothing), Mathf.Lerp(_lookVel.y, lookDest.y, 1.0f / _smoothing));
+        // Adjust lookPos beased on current look velocity
+        _lookPos += _lookVel * _lookSpeed;
+
+        // Clamp mouse look on the y axis
+        _lookPos.y = Mathf.Clamp(_lookPos.y, _vertLookExtents.x, _vertLookExtents.y);
+        
+        // set player rotation, y rotates head, x rotates player body
+        transform.rotation = Quaternion.AngleAxis(_lookPos.x, Vector3.up) * _rotOffset;
+        _head.localRotation = Quaternion.AngleAxis(_lookPos.y, Vector3.left) * _rotOffsetHead;
+    }
+
+    // Simple code for when you set the rotation of the player using other code
+    public void ResetRot()
+    {
+        _lookVel = new Vector2();
+        _lookPos = new Vector2();
+        _rotOffset = transform.rotation;
+        _rotOffsetHead = _head.transform.localRotation;
+    }
+    private void OnEnable()
+    {
+        _inputMap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputMap.Disable();
+    }
+}
