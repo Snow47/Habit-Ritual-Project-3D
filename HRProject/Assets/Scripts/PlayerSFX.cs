01f8feed00f2fff4b0c3d1dc4222e1fdaae2a07a
12ozJoCoke <squidprogames@gmail.com>
2023-03-29 18:29:25 -0700

    updated walking sfx script to make walking not sound as wacky

--- HRProject/Assets/Scripts/PlayerSFX.cs
+++ HRProject/Assets/Scripts/PlayerSFX.cs
@@ -7,12 +7,16 @@ public class PlayerSFX : MonoBehaviour
     PlayerMotor motor;
     public AudioClip walkingSFX;
     AudioSource asource;
+    string playstate;
 
     //Interval in seconds for how often the walking sfx plays
     public float walkSFXInterval;
 
+    //Lower threshold for if the walking sfx will play after player lets go of WASD, will be obsolete if raw WASD input can be read at some point
+    public float speedThreshold;
+
     float walkSFXtimer = 0;
-    bool isWalking = false;
+    public bool isWalking = false, gotToHighSpeed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,21 +29,36 @@ public class PlayerSFX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("Player is on ground = " + motor.IsOnGround);
-        //Debug.Log("Player speed = " + motor.CurrentSpeed);
-        if (motor.CurrentSpeed > 0.5f && motor.IsOnGround)
+        playstate = motor.CurrentState.ToString();
+        Debug.Log(playstate);
+        Debug.Log("Player speed = " + motor.CurrentSpeed);
+        if (motor.CurrentSpeed > 0.7f && playstate == "Grounded")
         {
-            if (!isWalking) asource.Play();
-            isWalking = true;
+            if (!gotToHighSpeed)
+            {
+                if (!isWalking) asource.Play();
+                isWalking = true;
+            }else
+            {
+                if (motor.CurrentSpeed >= speedThreshold && playstate == "Grounded")
+                {
+                    if (!isWalking) asource.Play();
+                    isWalking = true;
+                }else if (motor.CurrentSpeed < speedThreshold)
+                {
+                    gotToHighSpeed = false;
+                }
+            }
         }else
         {
-            asource.Stop();
+            gotToHighSpeed = false;
             isWalking = false;
             walkSFXtimer = 0;
         }
 
         if (isWalking)
         {
+            if (motor.CurrentSpeed >= speedThreshold) gotToHighSpeed = true;
             if (walkSFXtimer < walkSFXInterval)
             {
                 walkSFXtimer += Time.deltaTime;
